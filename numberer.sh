a8efc6a3c0e3071b9a122eb043fc79f304f71ecd
Bryan Clair <bryan@slu.edu>
2020-01-07 21:51:52 -0600

    Shell script now can fix/unfix.

--- numberer.sh
+++ numberer.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+if [ "$1" == "fix" ]
+then
+    echo "Converting @exercise. to numbers"
+elif [ "$1" == "unfix" ]
+then
+    echo "Converting numbered exercises back to @exercise."
+else
+    echo "Call this with either fix or unfix as an argument"
+    exit 1
+fi
+
 for chapter in *-*.Rmd
 do
     echo $chapter
-    python numberer.py fix $chapter > temp
+    python numberer.py $1 $chapter > temp
     mv temp $chapter
 done
