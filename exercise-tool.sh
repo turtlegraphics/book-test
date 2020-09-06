50ea075a90d5ef5d26a65307aa424095cb9751fe
Bryan Clair <bryan@slu.edu>
2020-09-06 13:37:00 -0500

    Updated tool to remove &nbsp; and add closing :::

--- exercise-tool.sh
+++ exercise-tool.sh
@@ -1,4 +1,12 @@
-# Find @label. at start of line and replace with
-# ::: {.exercise ex:label} <newline>
+#!/bin/bash
 
-sed -E $'s/^@([^.]*)\. /::: {.exercise #ex:\\1}\\\n/'
+# Replace @label. exercises with new fenced exercises
+
+# Find @label. at start of line and replace with
+# :::
+# 
+# ::: {.exercise ex:label}
+#
+cat "${1:-/dev/stdin}" |\
+grep -v '^[[:space:]]*&nbsp;[[:space:]]*$' |\
+sed -E $'s/^@([^.]*)\. /:::\\\n\\\n::: {.exercise #ex:\\1}\\\n/'
